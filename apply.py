90a99b4d6e5d378a3367e9e2eb98669815583f65
lens0021 <lorentz0021@gmail.com>
2020-01-08 03:48:24 +0900

    Implement Github Action

---
+++ apply.py
@@ -0,0 +1,44 @@
+import mwclient
+from os import environ
+from sys import argv, stdout
+from time import sleep
+from math import log
+import json
+import logging
+logging.basicConfig(stream=stdout, level=logging.DEBUG)
+
+PASSWORD = environ['FEMIWIKI_BOT_PASSWORD']
+
+TARGET_PATH = 'pages/'
+MODIFIED = [f for f in argv[1:] if f.startswith(TARGET_PATH)]
+
+
+def main():
+    if not MODIFIED:
+        logging.info('There is no modified file')
+        return
+
+    logging.info('modified files:' + ' / '.join(MODIFIED))
+
+    FEMIWIKI = mwclient.Site('femiwiki.com', path='/')
+    FEMIWIKI.login('페미위키_깃헙_가젯_봇@페미위키_깃헙_가젯_봇', PASSWORD)
+
+    SUMMARY = "Github @" + environ['GITHUB_ACTOR'] + "의 " + "https://github.com/" + \
+        environ['GITHUB_REPOSITORY'] + "/commit/" + environ['GITHUB_SHA'][:7]
+
+    for i, FILE in enumerate(MODIFIED):
+        f = open(FILE, "r")
+        title = FILE[len(TARGET_PATH):]
+        page = FEMIWIKI.pages[title]
+        logging.debug('Saving: '+title+'...')
+        page.save(f.read(), SUMMARY)
+        logging.debug('Saved: '+title)
+        time_to_sleep = log(i+1)
+        logging.debug('Sleep '+str(time_to_sleep)+' seconds...')
+        sleep(time_to_sleep)
+
+    # TODO: Handle deleted files
+
+
+if __name__ == '__main__':
+    main()
