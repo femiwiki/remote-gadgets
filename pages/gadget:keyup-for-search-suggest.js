7a19db9a516b2bc8b7c354a1a05cab1f761bf878
lens0021 <lorentz0021@gmail.com>
2020-02-12 21:50:00 +0900

    keyup-for-search-suggest: Change triggering method
    Closes #13

--- pages/gadget:keyup-for-search-suggest.js
+++ pages/gadget:keyup-for-search-suggest.js
@@ -2,19 +2,15 @@
 // Issue: https://github.com/femiwiki/mediawiki/issues/275
 "use strict";
 
-var $input = $("#searchInput"),
-  context = $input.data("suggestions-context");
-
-if (context !== undefined || context !== null) {
-  $input.keyup(function(e) {
-    switch (e.key) {
-      case "ArrowUp":
-      case "ArrowRight":
-      case "ArrowDown":
-      case "ArrowLeft":
-        return;
-    }
-    $.suggestions.keypress(e, context, context.data.keypressed);
-  });
-}
+var $input = $("#searchInput");
+$input.keyup(function(e) {
+  switch (e.key) {
+    case "ArrowUp":
+    case "ArrowRight":
+    case "ArrowDown":
+    case "ArrowLeft":
+      return;
+  }
+  $input.trigger("keypress");
+});
 // </nowiki>
