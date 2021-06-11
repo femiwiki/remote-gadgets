5f58f1a4c6b0893079de2d851975498d0a79a049
lens0021 <lorentz0021@gmail.com>
2021-06-12 08:36:49 +0900

    편집 중 사이트 공지 수정 버튼 숨김

--- gadgets/sitenotice-edit/gadget%3Asitenotice-edit.js
+++ gadgets/sitenotice-edit/gadget%3Asitenotice-edit.js
@@ -3,22 +3,13 @@
   var editButton = document.createElement('a');
   editButton.href = '/index.php?title=MediaWiki:Sitenotice&veaction=edit';
 
-  editButton.style.width = '1em';
-  editButton.style.height = '1em';
-  editButton.style.display = 'block';
-  editButton.style.backgroundSize = '100%';
-  editButton.style.backgroundImage =
-    "url('https://femiwiki-uploaded-files-thumb.s3.amazonaws.com/8/82/OOjs_UI_icon_edit-ltr-colored.svg/10px-OOjs_UI_icon_edit-ltr-colored.svg.png')";
+  editButton.classList.add('sitenotice-edit-button');
 
   var sitenotice = document.querySelector('#localNotice');
   var header = document.querySelector('.fw-header');
   if (sitenotice) {
-    editButton.style.float = 'right';
     sitenotice.insertBefore(editButton, sitenotice.firstChild);
   } else {
-    editButton.style.float = 'auto';
-    editButton.style.margin = 'auto';
-    editButton.style.marginTop = '1em';
     document.body.insertBefore(editButton, header);
   }
 })();
