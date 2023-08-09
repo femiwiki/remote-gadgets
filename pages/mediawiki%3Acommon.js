79d55b193e6da5b3f03e2f8262cbd993602f3e8b
lens0021 <lorentz0021@gmail.com>
2023-08-09 22:23:09 +0900

    gtag 추가
    https://github.com/femiwiki/femiwiki/issues/346

--- pages/mediawiki%3Acommon.js
+++ pages/mediawiki%3Acommon.js
@@ -1,1 +1,17 @@
 /* 이 자바스크립트 설정은 모든 문서, 모든 사용자에게 적용됩니다. */
+
+// PageViewInfoGA 비활성화로 인해 gtag를 common.js에서 대신 호출
+const gtagScript = document.createElement('script');
+gtagScript.setAttribute(
+  'src',
+  'https://www.googletagmanager.com/gtag/js?id=UA-82072330-1'
+);
+gtagScript.addEventListener('load', () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'UA-82072330-1');
+});
+document.getElementsByTagName('head')[0].appendChild(gtagScript);
