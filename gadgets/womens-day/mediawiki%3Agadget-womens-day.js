e09ea134494a5aad322fc63579101e394cd591a8
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-03-02 16:36:08 +0100

    work around chrome bug

--- gadgets/womens-day/mediawiki%3Agadget-womens-day.js
+++ gadgets/womens-day/mediawiki%3Agadget-womens-day.js
@@ -22,12 +22,25 @@
   element.hidden = true;
   document.body.append(element);
 
+  let x = 0;
+  let y = 0;
+
+  // 크롬 버그로 pointercancel에서 좌표를 받을 수 없음
+  // https://issues.chromium.org/issues/41320243
+  window.addEventListener('pointerdown', (ev) => {
+    if (ev.pointerType !== 'touch') {
+      return;
+    }
+    x = ev.x;
+    y = ev.y;
+  });
+
   window.addEventListener('pointercancel', (ev) => {
     if (ev.pointerType !== 'touch') {
       return;
     }
-    element.style.left = `${ev.x}px`;
-    element.style.top = `${ev.y}px`;
+    element.style.left = `${ev.x || x}px`;
+    element.style.top = `${ev.y || y}px`;
     element.hidden = false;
     if (!hasScrollEnd) {
       clearTimeout(setTimeoutHandle);
