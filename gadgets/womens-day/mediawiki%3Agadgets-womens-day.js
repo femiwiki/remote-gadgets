463aa784df3ec900f85a697d7351f70665dec673
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-03-02 14:52:16 +0100

    Add a custom cursor for women's day

--- gadgets/womens-day/mediawiki%3Agadgets-womens-day.js
+++ gadgets/womens-day/mediawiki%3Agadgets-womens-day.js
@@ -1,1 +1,15 @@
-//
+// <nowiki>
+/**
+ * [[틀:스포일러]] 를 보조합니다
+ */
+"use strict";
+(() => {
+  const today = new Date();
+
+  if (today.getMonth() != 2 || today.getDate() != 8) {
+    return;
+  }
+
+  document.body.classList.add("womens-day");
+})();
+// </nowiki>
