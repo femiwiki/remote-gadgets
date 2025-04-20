b59bf9251d782bd24c1fc68f6cc98a8aa73d8425
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-04-20 13:11:38 +0200

    npm run fix

--- gadgets/discord-widget/mediawiki%3Agadget-discord-widget.js
+++ gadgets/discord-widget/mediawiki%3Agadget-discord-widget.js
@@ -3,7 +3,9 @@
 (async () => {
   await mw.loader.getScript('https://cdn.jsdelivr.net/npm/@widgetbot/crate@3');
 
-  const rem = parseFloat(getComputedStyle(document.documentElement,null).fontSize)
+  const rem = parseFloat(
+    getComputedStyle(document.documentElement, null).fontSize
+  );
 
   new Crate({
     server: '314953743185477644',
@@ -15,9 +17,9 @@
     defer: true,
 
     // femiwiki color
-    color: "#aca6e4",
+    color: '#aca6e4',
 
-    location: [-rem * 6, -rem * 1]
+    location: [-rem * 6, -rem * 1],
   });
 })();
 // </nowiki>
