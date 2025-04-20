52fe49341f1972fe95767b6e2cf4edb122a4483c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-04-20 14:41:56 +0200

    fix: use pre-ES2017 syntax for discord widget

--- gadgets/discord-widget/mediawiki%3Agadget-discord-widget.js
+++ gadgets/discord-widget/mediawiki%3Agadget-discord-widget.js
@@ -1,25 +1,27 @@
 // <nowiki>
 'use strict';
-(async () => {
-  await mw.loader.getScript('https://cdn.jsdelivr.net/npm/@widgetbot/crate@3');
+(() => {
+  mw.loader
+    .getScript('https://cdn.jsdelivr.net/npm/@widgetbot/crate@3')
+    .then(() => {
+      const rem = parseFloat(
+        getComputedStyle(document.documentElement, null).fontSize
+      );
 
-  const rem = parseFloat(
-    getComputedStyle(document.documentElement, null).fontSize
-  );
+      new Crate({
+        server: '314953743185477644',
 
-  new Crate({
-    server: '314953743185477644',
+        // 공개잡담방
+        channel: '314953743185477644',
 
-    // 공개잡담방
-    channel: '314953743185477644',
+        // Do not load Discord until user clicks the widget
+        defer: true,
 
-    // Do not load Discord until user clicks the widget
-    defer: true,
+        // femiwiki color
+        color: '#aca6e4',
 
-    // femiwiki color
-    color: '#aca6e4',
-
-    location: [-rem * 6, -rem * 1],
-  });
+        location: [-rem * 6, -rem * 1],
+      });
+    });
 })();
 // </nowiki>
