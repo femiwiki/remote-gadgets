7f021ab479ab275a9ba0a7b6a18505e4e64e67a1
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-04-20 12:03:24 +0000

    feat: Add discord widget (#230)
    * Add discord widget
    
    * prettier
    
    * above the potential existing button
    
    * npm run fix

---
+++ gadgets/discord-widget/mediawiki%3Agadget-discord-widget.js
@@ -0,0 +1,25 @@
+// <nowiki>
+'use strict';
+(async () => {
+  await mw.loader.getScript('https://cdn.jsdelivr.net/npm/@widgetbot/crate@3');
+
+  const rem = parseFloat(
+    getComputedStyle(document.documentElement, null).fontSize
+  );
+
+  new Crate({
+    server: '314953743185477644',
+
+    // 공개잡담방
+    channel: '314953743185477644',
+
+    // Do not load Discord until user clicks the widget
+    defer: true,
+
+    // femiwiki color
+    color: '#aca6e4',
+
+    location: [-rem * 6, -rem * 1],
+  });
+})();
+// </nowiki>
